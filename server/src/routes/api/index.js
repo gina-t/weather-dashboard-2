82d7c95f121923c1c41a711c163f98025d9bcf4f
Tao <ginadrcoder@gmail.com>
2025-01-04 09:45:25 +1100

    modified server/weatherService.ts and client/main.ts

--- server/src/routes/api/index.js
+++
@@ -1,6 +0,0 @@
-// this file sets up the router for the /weather endpoint.
-import { Router } from 'express';
-const router = Router();
-import weatherRoutes from './weatherRoutes.js';
-router.use('/weather', weatherRoutes);
-export default router;
